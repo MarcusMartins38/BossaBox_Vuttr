57e09cfa3e506754577394f9c96b406e1679264d
Marcus Martins <marcusmartins38@gmail.com>
2020-07-09 13:07:35 -0400

    Getting datas from api

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import api from "../../services/api";
 
 import { Container, HeaderOfList, InputSearchDiv } from "./styles";
 
 import IconLupa from "../../assets/IconLupa.svg";
 import IconPlus from "../../assets/Plus.svg";
 
+interface PropsApi {
+  id: number;
+  title: string;
+  link: string;
+  description: string;
+  tags: string[];
+}
+
 const Home: React.FC = () => {
+  const [tools, setTools] = useState<PropsApi[]>([]);
+
+  useEffect(() => {
+    api.get("/tools").then((response) => {
+      setTools(response.data);
+      console.log(response.data);
+    });
+  }, []);
+
   return (
     <Container>
       <h1>Vuttr</h1>
