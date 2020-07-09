1a467c18b8ddf4501845748ae1bec6f22f6e6156
Marcus Martins <marcusmartins38@gmail.com>
2020-07-09 14:43:50 -0400

    Rendering the API

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,20 @@
 import React, { useState, useEffect } from "react";
+import { TiDeleteOutline, TiPlus } from "react-icons/ti";
+import { FaHashtag } from "react-icons/fa";
 
 import api from "../../services/api";
 
-import { Container, HeaderOfList, InputSearchDiv } from "./styles";
+import {
+  Container,
+  Content,
+  HeaderOfList,
+  InputSearchDiv,
+  BoxOfApi,
+  Information,
+  HeaderOfContent,
+} from "./styles";
 
 import IconLupa from "../../assets/IconLupa.svg";
-import IconPlus from "../../assets/Plus.svg";
 
 interface PropsApi {
   id: number;
@@ -27,22 +36,48 @@ const Home: React.FC = () => {
 
   return (
     <Container>
-      <h1>Vuttr</h1>
-      <h2>Very Useful Tools to Remember</h2>
+      <Content>
+        <h1>Vuttr</h1>
+        <h2>Very Useful Tools to Remember</h2>
 
-      <HeaderOfList>
-        <div>
-          <InputSearchDiv>
-            <img src={IconLupa} alt="Lupa" />
-            <input type="text" placeholder="Search" />
-          </InputSearchDiv>
-          <input type="checkbox" />
-          <label>Search in tags only</label>
-        </div>
-        <button>
-          <img src={IconPlus} alt="IconPlus" /> Add
-        </button>
-      </HeaderOfList>
+        <HeaderOfList>
+          <div>
+            <InputSearchDiv>
+              <img src={IconLupa} alt="Lupa" />
+              <input type="text" placeholder="Search" />
+            </InputSearchDiv>
+            <input type="checkbox" />
+            <label>Search in tags only</label>
+          </div>
+          <button>
+            <TiPlus /> Add
+          </button>
+        </HeaderOfList>
+
+        {tools.map((tool) => (
+          <BoxOfApi key={tool.id}>
+            <Information>
+              <HeaderOfContent>
+                <a href={tool.link}>{tool.title}</a>
+                <div>
+                  <button>
+                    <TiDeleteOutline size={32} />
+                  </button>
+                  <p>remove</p>
+                </div>
+              </HeaderOfContent>
+
+              <p id="description">{tool.description}</p>
+
+              <div>
+                {tool.tags.map((tag) => (
+                  <strong key={tag}>#{tag}</strong>
+                ))}
+              </div>
+            </Information>
+          </BoxOfApi>
+        ))}
+      </Content>
     </Container>
   );
 };
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,15 +1,18 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  height: 100vh;
   width: 100%;
   max-width: 900px;
 
+  margin: 0 auto;
+`;
+
+export const Content = styled.div`
+  width: 100%;
+  margin-top: 64px;
+
   display: flex;
   flex-direction: column;
-  margin: 0 auto;
-
-  border: 1px solid black;
 
   h1 {
     text-transform: uppercase;
@@ -43,22 +46,26 @@ export const HeaderOfList = styled.div`
   }
 
   button {
-    width: 114px;
-    height: 32px;
+    padding: 12px 40px;
+    background-color: #5b44ee;
+    color: white;
 
     border: 1px solid black;
     border-radius: 6px;
 
-    font-size: 16px;
+    font-size: 18px;
 
     display: flex;
     justify-items: center;
     justify-content: center;
 
-    img {
-      margin-right: 5px;
-      max-width: 15px;
-      max-height: 15px;
+    cursor: pointer;
+    transition: background-color 0.5s;
+
+    &:hover {
+      background-color: #20127d;
+    }
+    &:active {
     }
   }
 `;
@@ -86,5 +93,69 @@ export const InputSearchDiv = styled.div`
     padding-left: 8px;
 
     font-size: 16px;
+  }
+`;
+
+export const BoxOfApi = styled.div`
+  width: 100%;
+  min-height: 150px;
+
+  margin-top: 20px;
+  margin-bottom: 30px;
+
+  border: 2px solid black;
+`;
+
+export const HeaderOfContent = styled.div`
+  display: flex;
+
+  justify-content: space-between;
+
+  a {
+    font-size: 32px;
+    color: #4129e0;
+    transition: font-size 0.5s;
+
+    &:hover {
+      font-size: 36px;
+    }
+  }
+
+  div {
+    display: flex;
+    align-items: center;
+
+    button {
+      background-color: transparent;
+      border: 0;
+      cursor: pointer;
+
+      svg {
+        color: #fb3741;
+        transition: color 0.5s;
+
+        &:hover {
+          color: #a0030b;
+        }
+      }
+    }
+  }
+`;
+
+export const Information = styled.div`
+  width: 95%;
+
+  margin: 12px auto 0 auto;
+
+  #description {
+    margin-top: 16px;
+  }
+
+  div {
+    margin-top: 8px;
+
+    strong {
+      margin-right: 8px;
+    }
   }
 `;
